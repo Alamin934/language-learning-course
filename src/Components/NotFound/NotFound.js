da4c6b517e0e055f644e5472023c3d50a1092224
Alamin934 <islamalamin934@gmail.com>
2021-10-04 19:03:41 +0600

    Edit Readme file

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import notFoundImg from '../../images/404.jpg';
 
 const NotFound = () => {
     return (
-        <div>
-            This is 404 page
+        <div className="pb-3">
+            {/* 404  */}
+            <div className="w-50 mx-auto">
+                <img className="img-fluid" src={notFoundImg} alt="" />
+            </div>
+            {/* Back to Home Button */}
+            <Link to="/home" className="nav-link">
+                <button className="btn-grad mx-auto">Back to Home</button>
+            </Link>
         </div>
     );
 };
